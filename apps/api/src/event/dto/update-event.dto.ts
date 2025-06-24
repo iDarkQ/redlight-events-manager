4260b0dcbb6bef20ce3a71b98ed48f7a0ab6cffd
iDarkQ <matikuki3@gmail.com>
2025-06-24 13:20:15 +0100

    refactor(update-event.dto.ts): add properties required to update event

--- apps/api/src/event/dto/update-event.dto.ts
+++ apps/api/src/event/dto/update-event.dto.ts
@@ -1,4 +1,33 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateEventDto } from './create-event.dto';
+import { IsString, IsOptional, IsDate, IsInt, IsLatitude, IsLongitude } from "class-validator";
+import { Type } from "class-transformer";
 
-export class UpdateEventDto extends PartialType(CreateEventDto) {}
+export class UpdateEventDto {
+    @IsOptional()
+    @IsString()
+    title?: string;
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsOptional()
+    @Type(() => Date)
+    @IsDate()
+    date?: Date;
+
+    @IsOptional()
+    @IsString()
+    type?: string;
+
+    @IsOptional()
+    @IsInt()
+    maxParticipants?: number;
+
+    @IsOptional()
+    @IsLatitude()
+    latitude?: number;
+
+    @IsOptional()
+    @IsLongitude()
+    longitude?: number;
+}
