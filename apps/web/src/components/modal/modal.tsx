36d47e62bb03a91fe425f5a1ea453b0c81678a56
iDarkQ <matikuki3@gmail.com>
2025-06-26 14:36:43 +0100

    feat(modal): create modal component

---
+++ apps/web/src/components/modal/modal.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+import { styles } from ".";
+import clsx from "clsx";
+import { IoMdClose } from "react-icons/io";
+import { IconButton } from "~/components/icon-button";
+
+interface ModalProps {
+  open: boolean;
+  title?: string;
+  onClose?: () => void;
+  children?: ReactNode;
+}
+
+export const Modal = ({ children, title = "Modal", open, onClose }: ModalProps) => (
+  <div className={clsx(styles.modal, !open && styles.closed)} onClick={onClose}>
+    <div className={styles.content} onClick={(e) => e.stopPropagation()}>
+      <div className={styles.header}>
+        <h3>{title}</h3>
+        <IconButton onClick={onClose}>
+          <IoMdClose className={styles.icon} />
+        </IconButton>
+      </div>
+      {children}
+    </div>
+  </div>
+);
