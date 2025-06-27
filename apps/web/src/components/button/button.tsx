4fe7a0afc8e7baa093bb54484bf4e9d6eea513a2
iDarkQ <matikuki3@gmail.com>
2025-06-27 14:48:59 +0100

    refactor(button): add options to set loading status and change color

--- apps/web/src/components/button/button.tsx
+++ apps/web/src/components/button/button.tsx
@@ -1,14 +1,28 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 import { styles } from ".";
 import clsx from "clsx";
+import { LoadingIndicator } from "~/components/loading-indicator";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   children?: ReactNode;
+  loading?: boolean;
+  color?: "red" | "white";
 }
 
-export const Button = ({ className, children, ...props }: ButtonProps) => (
-  <button className={clsx(styles.button, className && className)} {...props}>
-    {children}
+export const Button = ({
+  className,
+  children,
+  color = "red",
+  loading = false,
+  ...props
+}: ButtonProps) => (
+  <button
+    className={clsx(styles.button, styles[color], loading && styles.loading, className && className)}
+    disabled={loading || props.disabled}
+    {...props}
+  >
+    {!loading && children}
+    {loading && <LoadingIndicator />}
   </button>
 );
