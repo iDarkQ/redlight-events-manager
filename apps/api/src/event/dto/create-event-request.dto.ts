3115090d311d16f7f5797567f233f488d12e4ac2
iDarkQ <matikuki3@gmail.com>
2025-06-29 17:24:45 +0100

    refactor(dto): utilize EventDto to create specific dtos

--- apps/api/src/event/dto/create-event-request.dto.ts
+++ apps/api/src/event/dto/create-event-request.dto.ts
@@ -1,8 +1,9 @@
 import { OmitType } from "@nestjs/swagger";
 import { EventDto } from "src/event/dto/event.dto";
 
-export class CreateEventDto extends OmitType(EventDto, [
+export class CreateEventRequestDto extends OmitType(EventDto, [
     "id",
+    "creatorId",
     "createdAt",
     "participants",
 ] as const) {}
