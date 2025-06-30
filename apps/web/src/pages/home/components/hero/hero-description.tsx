7535e8093033d148a36d66322cead07008ee304c
iDarkQ <matikuki3@gmail.com>
2025-06-30 18:03:11 +0100

    refactor(hero-descriptions): slight changes to content

--- apps/web/src/pages/home/components/hero/hero-description.tsx
+++ apps/web/src/pages/home/components/hero/hero-description.tsx
@@ -1,8 +1,8 @@
-import {styles} from ".";
+import { styles } from ".";
 
 export const HeroDescription = () => (
   <p className={styles.description}>
-    Get up‑to‑date, time‑zone accurate schedules of all Redlight employee sporting events, complete
-    with links to location, times, and more—designed exclusively for Redlight's team.
+    Get up‑to‑date, accurate schedules of all Redlight employee sporting events, complete with
+    locations, times, participants, and more—designed exclusively for Redlight's team.
   </p>
 );
