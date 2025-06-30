9942ac338dca942c1530a367d25ee482dd7482da
iDarkQ <matikuki3@gmail.com>
2025-06-30 17:56:30 +0100

    refactor(icon-button): utilize Icon component to create icon button

--- apps/web/src/components/icon-button/icon-button.tsx
+++ apps/web/src/components/icon-button/icon-button.tsx
@@ -1,6 +1,7 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 import { styles } from ".";
 import clsx from "clsx";
+import { Icon } from "~/components/icon";
 
 interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
@@ -8,6 +9,16 @@ interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   animation?: boolean;
 }
 
-export const IconButton = ({ children, className, animation = true, ...props }: IconButtonProps) => (
-  <button className={clsx(styles.button, animation && styles.animation, className && className)} {...props}>{children}</button>
+export const IconButton = ({
+  children,
+  className,
+  animation = true,
+  ...props
+}: IconButtonProps) => (
+  <button
+    className={clsx(styles.button, className && className)}
+    {...props}
+  >
+    <Icon animation={animation}>{children}</Icon>
+  </button>
 );
