c2fc5fe8d8185e9d7d6c999739076af47f36303e
iDarkQ <matikuki3@gmail.com>
2025-06-30 18:04:49 +0100

    feat(routes.ts): add routes for viewing, editing and creating event

--- apps/web/src/utils/routes.ts
+++ apps/web/src/utils/routes.ts
@@ -3,3 +3,7 @@ export enum Routes {
     LOGIN = "/login",
     EVENT = "/event",
 }
+
+export const routeViewEvent = (eventId: string) => `${Routes.EVENT}/view/${eventId}`;
+export const routeEditEvent = (eventId: string) => `${Routes.EVENT}/edit/${eventId}`;
+export const routeCreateEvent = () => `${Routes.EVENT}/create`;
