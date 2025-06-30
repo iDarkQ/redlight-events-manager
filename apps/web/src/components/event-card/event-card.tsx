4d186ac24bea02207c9ff99408fe4b54c0617be7
iDarkQ <matikuki3@gmail.com>
2025-06-30 17:56:10 +0100

    refactor(event-card.tsx): replace date and title with events data

--- apps/web/src/components/event-card/event-card.tsx
+++ apps/web/src/components/event-card/event-card.tsx
@@ -3,18 +3,26 @@ import { styles } from ".";
 import Football from "~/assets/images/football.png";
 import clsx from "clsx";
 import { FaLocationDot } from "react-icons/fa6";
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+import { EventDto } from "~/lib/api";
+
+dayjs.extend(advancedFormat);
+dayjs.extend(localizedFormat);
 
 interface EventCardProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
+  event: EventDto;
 }
 
-export const EventCard = ({ className, ...props }: EventCardProps) => (
+export const EventCard = ({ event, className, ...props }: EventCardProps) => (
   <button className={clsx(styles.card, className && className)} {...props}>
     <img className={styles.image} src={Football} alt="Avatar" />
     <div className={styles.container}>
-      <p>Sunday 12th April | 10:00 am</p>
+      <p>{dayjs(event.date).format("dddd Do MMMM | hh:mm a")}</p>
       <h4>
-        <b>Football Match</b>
+        <b>{event.title}</b>
       </h4>
       <p>
         <FaLocationDot />
