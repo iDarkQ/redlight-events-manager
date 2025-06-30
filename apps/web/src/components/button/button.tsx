58f3a13932f4661958ee6a28f4b53d1caf9d3f03
iDarkQ <matikuki3@gmail.com>
2025-06-30 17:55:14 +0100

    refactor(button.tsx): make sure onClick will be detected and cause loading animation if async

--- apps/web/src/components/button/button.tsx
+++ apps/web/src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode, useState } from "react";
 import { styles } from ".";
 import clsx from "clsx";
 import { LoadingIndicator } from "~/components/loading-indicator";
@@ -6,23 +6,46 @@ import { LoadingIndicator } from "~/components/loading-indicator";
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   children?: ReactNode;
-  loading?: boolean;
   color?: "red" | "white";
+  loading?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void | Promise<void>;
 }
 
 export const Button = ({
   className,
   children,
   color = "red",
-  loading = false,
+  onClick,
+  disabled,
+  loading: loadingProp,
   ...props
-}: ButtonProps) => (
-  <button
-    className={clsx(styles.button, styles[color], loading && styles.loading, className && className)}
-    disabled={loading || props.disabled}
-    {...props}
-  >
-    {!loading && children}
-    {loading && <LoadingIndicator />}
-  </button>
-);
+}: ButtonProps) => {
+  const [internalLoading, setInternalLoading] = useState(false);
+  const loading = loadingProp ?? internalLoading;
+
+  // HACK: Detect whether button onClick is happening and cause loading animation
+  const handleClick = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (!onClick) return;
+    const result = onClick(event);
+    if (result instanceof Promise && loadingProp === undefined) {
+      setInternalLoading(true);
+      try {
+        await result;
+      } finally {
+        setInternalLoading(false);
+      }
+    }
+  };
+
+  return (
+    <button
+      className={clsx(styles.button, styles[color], (loading || disabled) && styles.loading, className && className)}
+      disabled={loading || disabled}
+      onClick={handleClick}
+      {...props}
+    >
+      {!loading && children}
+      {loading && <LoadingIndicator />}
+    </button>
+  );
+};
