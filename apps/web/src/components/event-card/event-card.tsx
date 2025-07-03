e9668892304ea3e1b321f9fc9ef6abf21ea4b5fb
iDarkQ <matikuki3@gmail.com>
2025-07-03 23:12:34 +0100

    refactor(event-card): display real banner and location name

--- apps/web/src/components/event-card/event-card.tsx
+++ apps/web/src/components/event-card/event-card.tsx
@@ -1,15 +1,10 @@
 import { ButtonHTMLAttributes } from "react";
 import { styles } from ".";
-import Football from "~/assets/images/football.png";
 import clsx from "clsx";
 import { FaLocationDot } from "react-icons/fa6";
-import dayjs from "dayjs";
-import advancedFormat from "dayjs/plugin/advancedFormat";
-import localizedFormat from "dayjs/plugin/localizedFormat";
 import { EventDto } from "~/lib/api";
-
-dayjs.extend(advancedFormat);
-dayjs.extend(localizedFormat);
+import { eventCardDateFormat } from "~/utils/date";
+import { baseUrl } from "~/utils/url";
 
 interface EventCardProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
@@ -18,15 +13,24 @@ interface EventCardProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export const EventCard = ({ event, className, ...props }: EventCardProps) => (
   <button className={clsx(styles.card, className && className)} {...props}>
-    <img className={styles.image} src={Football} alt="Avatar" />
+    <div className={styles.thumbnail}>
+      {event.banner ? (
+        <img className={styles.image} src={baseUrl + "/" + event.banner} alt="Banner" />
+      ) : (
+        <div className={styles.banner}>
+          <h3>Sport Event</h3>
+        </div>
+      )}
+      <span className={styles.view}>View Event</span>
+    </div>
     <div className={styles.container}>
-      <p>{dayjs(event.date).format("dddd Do MMMM | hh:mm a")}</p>
+      <p>{eventCardDateFormat(event.date)}</p>
       <h4>
         <b>{event.title}</b>
       </h4>
       <p>
         <FaLocationDot />
-        Coimbra
+        {event.location}
       </p>
     </div>
   </button>
