54d8adf0c44f32f4391fbaf9dde1b3be8878e53b
iDarkQ <matikuki3@gmail.com>
2025-07-04 15:50:48 +0100

    refactor(react-env.d.ts): change declaration for module css by adding classes

--- apps/web/src/types/react-env.d.ts
+++ apps/web/src/types/react-env.d.ts
@@ -1,4 +1,8 @@
-declare module "*.css";
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
 declare module "*.png";
 
 declare module "*.svg?react" {
