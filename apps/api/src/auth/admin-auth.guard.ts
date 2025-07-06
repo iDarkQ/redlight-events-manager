accff4f1ad505c519a5e298ef08b8e03c22e36c9
iDarkQ <matikuki3@gmail.com>
2025-07-06 17:05:26 +0100

    feat(admin-auth.guard.ts): create admin auth guard

---
+++ apps/api/src/auth/admin-auth.guard.ts
@@ -0,0 +1,51 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    ForbiddenException,
+    Injectable,
+    UnauthorizedException,
+} from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
+import { PrismaService } from "~/prisma.service";
+
+@Injectable()
+export class AdminAuthGuard implements CanActivate {
+    constructor(
+        private jwtService: JwtService,
+        private prisma: PrismaService,
+    ) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const token = this.extractTokenFromHeader(request);
+        if (!token) {
+            throw new UnauthorizedException();
+        }
+        try {
+            const payload = await this.jwtService.verifyAsync(token, {
+                secret: process.env.JWT_SECRET,
+            });
+
+            const user = await this.prisma.user.findFirstOrThrow({ where: { id: payload.uId } });
+
+            if (user.banned) {
+                throw new ForbiddenException("This user is banned");
+            }
+
+            if (user.role !== "ADMIN") {
+                throw new UnauthorizedException("You need to be an admin to do it");
+            }
+
+            request["user"] = user;
+        } catch {
+            throw new UnauthorizedException("Could not authenticate");
+        }
+        return true;
+    }
+
+    private extractTokenFromHeader(request: Request): string | undefined {
+        const [type, token] = request.headers.authorization?.split(" ") ?? [];
+        return type === "Bearer" ? token : undefined;
+    }
+}
