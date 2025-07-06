1d4fe68c55f0f56e7d449d9de2d2c894329a02cc
iDarkQ <matikuki3@gmail.com>
2025-07-06 17:11:58 +0100

    refactor(event): rework logic behind use event and simplify it

--- apps/web/src/pages/event/event.tsx
+++ apps/web/src/pages/event/event.tsx
@@ -2,24 +2,13 @@ import { PageLoading } from "~/components/page-loading";
 import { Details } from "./components/details";
 import { Hero } from "./components/hero";
 import { useEvent } from ".";
-import { useUser } from "~/providers/user";
-import { useMessage } from "~/providers/message";
-import { Navigate } from "react-router";
-import { Routes } from "~/utils/routes";
 
 export interface EventProps {
   state: "edit" | "create" | "view";
 }
 
 export const Event = ({ state }: EventProps) => {
-  const { user } = useUser();
-  const {showMessage} = useMessage();
   const { isLoading, selectedEvent } = useEvent({ state });
-
-  if (!user && state !== "view") {
-    showMessage("You need to be logged in to do that", "error");
-    return <Navigate to={Routes.HOME} />;
-  }
 
   return (
     <main>
--- apps/web/src/pages/event/use-event.tsx
+++ apps/web/src/pages/event/use-event.tsx
@@ -1,41 +1,56 @@
 import { EventProps } from "~/pages/event/event";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useEvent as useEventProvider } from "~/providers/event";
 import { useUser } from "~/providers/user";
-const alreadyLoadedIds = new Set<string>();
+import { Routes } from "~/utils/routes";
+import { useMessage } from "~/providers/message";
 
 export const useEvent = ({ state }: EventProps) => {
   const { id } = useParams<{ id: string }>();
-  const { setSelectedEvent, selectedEvent, events } = useEventProvider();
-  const { user } = useUser();
+  const { setSelectedEvent, selectedEvent, events, fetched } = useEventProvider();
+  const { user, fetched: userFetched } = useUser();
+  const navigate = useNavigate();
+  const { showMessage } = useMessage();
   const [showLoader, setShowLoader] = useState(true);
 
   useEffect(() => {
-    if (!id) return;
-    if (state === "create" || alreadyLoadedIds.has(id)) {
-      setShowLoader(false);
-    } else {
-      setShowLoader(true);
-    }
-  }, [id, state]);
-
-  useEffect(() => {
-    if (!id) return;
-    const event = events.find((e) => e.id === id);
-    if (!event) return;
-    if (state === "edit" && event.creatorId !== user!.id && user!.role !== "ADMIN") {
+    if (!userFetched) return;
+    if (state !== "view" && !user) {
+      showMessage("You don't have permissions to do it", "error");
+      navigate(Routes.HOME);
       return;
     }
+  }, [userFetched, user]);
+
+  useEffect(() => {
+    if (!id || !fetched || !userFetched || state === "create") return;
+
+    const event = events.find((e) => e.id === id);
+    if (!event) {
+      showMessage("This event does not exist", "error");
+      navigate(Routes.HOME);
+      return;
+    }
+
+    if (state === "edit") {
+      if (user && event.creatorId !== user!.id && user!.role !== "ADMIN") {
+        showMessage("You don't have permissions to do it", "error");
+        navigate(Routes.HOME);
+        return;
+      }
+    }
+
     setSelectedEvent(event);
-  }, [id, events, state, setSelectedEvent, user]);
+  }, [id, events, state, setSelectedEvent, user, userFetched]);
 
   useEffect(() => {
     if (state === "create" || !selectedEvent || !showLoader) return;
+
     const t = setTimeout(() => {
       setShowLoader(false);
-      alreadyLoadedIds.add(id!);
     }, 2500);
+
     return () => clearTimeout(t);
   }, [selectedEvent, showLoader, state, id]);
 
