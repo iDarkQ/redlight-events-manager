a9c2b77ff604ea0db36725c53e771ff70ae8ac2c
iDarkQ <matikuki3@gmail.com>
2025-07-06 17:07:08 +0100

    refactor(avatar): add user and className properties

--- apps/web/src/components/avatar/avatar-user.tsx
+++ apps/web/src/components/avatar/avatar-user.tsx
@@ -5,10 +5,12 @@ import { baseUrl } from "~/utils/url";
 interface AvatarUserProps {
   name: string;
   profile: string | null;
+  className?: string;
+  onClick?: () => void;
 }
 
-export const AvatarUser = ({ name, profile }: AvatarUserProps) => (
-  <Avatar title={name} size="large">
+export const AvatarUser = ({ name, profile, className, onClick }: AvatarUserProps) => (
+  <Avatar title={name} size="large" onClick={onClick} className={className}>
     {profile ? (
       <img src={baseUrl + "/" + profile} alt={name} className={styles.avatar} />
     ) : (
